0445210a6a8c2c486d9167eeca16fa00123ade98
Ashwin <arihant2math@gmail.com>
2020-05-18 21:52:54 -0700

    Python package readyness and pypi installation initiation

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    version="2.3",
+    author="Ashwin Naren",
+    author_email="arihant2math@gmail.com",
+    description="A large number theory package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
