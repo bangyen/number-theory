40eac885ab98168588ff59a27b4953ce8e4e604c
Ashwin <arihant2math@gmail.com>
2020-05-28 13:03:08 -0700

    started making easy documentation

---
+++ __init__.py
@@ -0,0 +1,6 @@
+import
+print("to import the python libraries in this package import:"
+      "number_theory_essentials"
+      "number_theory"
+      "numbertheorylists"
+      "for documentation purposes import help")
