8c619b844b0eaf4419ed908dd82064ecc204536c
Ashwin <58895652+arihant2math@users.noreply.github.com>
2020-06-08 18:03:10 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="number-theory", # Replace with your own username
+    version="2.4",
+    author="Ashwin Naren",
+    author_email="arihant2math@gmail.com",
+    description="A large number theory package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
