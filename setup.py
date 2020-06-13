996063737cced07b2cf4e5a2bb708303c74d83f5
Ashwin <arihant2math@gmail.com>
2020-06-13 13:17:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-<<<<<<< HEAD
-    name="ashwin06", # Replace with your own username
+    name="ashwin06",
     version="2.4",
-=======
-    name="number-theory", # Replace with your own username
-    version="2.3",
->>>>>>> 00f964b4ae4fe2f2828cfa0885574f5a70c5ffb4
     author="Ashwin Naren",
     author_email="arihant2math@gmail.com",
     description="A large number theory package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/",
+    url="https://github.com/ari/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
