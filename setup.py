a92c6ea3753d82f46bf80e26a4a39597c552ddf3
Ashwin <arihant2math@gmail.com>
2020-07-01 11:18:06 -0700

    no venv
    no venv

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ashwin06",
+    name="math-basicgit ",
     version="2.4",
     author="Ashwin Naren",
     author_email="arihant2math@gmail.com",
     description="A large number theory package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ari/",
+    url="https://github.com/arihant2math/number-theory",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
